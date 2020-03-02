5c24ee44452df53f232ca1dd765e76c8ba8c74fe
Josh Ellis <joshe523@gmail.com>
2020-03-02 22:53:04 +0000

    Automatically backed up by Learn

--- deli_counter.rb
+++ deli_counter.rb
@@ -2,4 +2,6 @@ require 'pry'
 
 def line(people)
   puts "The line is currently empty." if people.size == 0
+  puts "The line is currently: "
+  people.each_with_index { |name, idx| puts "#{idx+1} #{name}" }
 end
